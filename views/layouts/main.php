2202450d10bad6c5c3c84bd59bcc7b55511501a6
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-02-07 09:42:47 +0200

    Remove navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -24,29 +24,6 @@ AppAsset::register($this);
 
 <?php $this->beginBody() ?>
 	<div class="wrap">
-		<?php
-			NavBar::begin([
-				'brandLabel' => 'My Company',
-				'brandUrl' => Yii::$app->homeUrl,
-				'options' => [
-					'class' => 'navbar-inverse navbar-fixed-top',
-				],
-			]);
-			echo Nav::widget([
-				'options' => ['class' => 'navbar-nav navbar-right'],
-				'items' => [
-					['label' => 'Home', 'url' => ['/site/index']],
-					['label' => 'About', 'url' => ['/site/about']],
-					['label' => 'Contact', 'url' => ['/site/contact']],
-					Yii::$app->user->isGuest ?
-						['label' => 'Login', 'url' => ['/site/login']] :
-						['label' => 'Logout (' . Yii::$app->user->identity->username . ')' ,
-							'url' => ['/site/logout'],
-							'linkOptions' => ['data-method' => 'post']],
-				],
-			]);
-			NavBar::end();
-		?>
 
 		<div class="container">
 			<?= Breadcrumbs::widget([
