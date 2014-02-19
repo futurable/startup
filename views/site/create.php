25dec1f383b8f0205e2fd16ef5da5cdcda858b05
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-02-19 16:12:17 +0200

    Modify form

--- views/site/create.php
+++ views/site/create.php
@@ -5,6 +5,8 @@
  */
 use kartik\widgets\ActiveForm;
 use yii\helpers\Html;
+use app\models\CostbenefitItem;
+use app\commands\CBCTableRow;
 $this->title = 'Futural startup - '.Yii::t('Company', 'Create a company');
 ?>
 <div class="site-index">
@@ -15,23 +17,42 @@ $this->title = 'Futural startup - '.Yii::t('Company', 'Create a company');
 		<?php	
 			$form = ActiveForm::begin([
 				'id' => 'create-form'
-				, 'type' => ActiveForm::TYPE_HORIZONTAL
+				, 'type' => ActiveForm::TYPE_INLINE
 			]);
-
-			echo '<div class="form-group">';
-				echo $form->field($company, 'name');
-				echo $form->field($company, 'email');
-				echo $form->field($company, 'industry');
-				echo $form->field($company, 'employees');
-			echo '</div>';
-			
-			echo '<div class="form-group">';
-				
-			echo '</div>';
-			
-			echo Html::submitButton(Yii::t('Company', 'Create'), ['class' => 'btn btn-success']);
-			ActiveForm::end();
 		?>
+		
+		<div class="form-group">
+			<h2><?php echo Yii::t('Company', 'Info'); ?></h2>
+				
+			<?php echo $form->field($company, 'name'); ?><br/>
+			<?php echo $form->field($company, 'email'); ?><br/>
+			<?php echo $form->field($company, 'industry_id'); ?><br/>
+			<?php echo $form->field($company, 'employees'); ?><br/>
+		</div>
+	
+		<div class="form-group">
+			<table id='costBenefitCalculationTable'>
+				<tr>
+					<th></th>
+					<th><?php echo  Yii::t('Company', 'Monthly') ?> (&euro;)</th>
+				  	<th><?php echo  Yii::t('Company', 'Yearly') ?> (&euro;)</th>
+					<th></th>
+				</tr>
+				<?php
+					echo "<h2>".Yii::t('Company', 'Cost-benefit calculation')."</h2><br/>";
+					foreach($costBenefitItemTypes as $costBenefitItemType){
+						echo CBCTableRow::getRow($form, $costBenefitItemType); 
+					}
+				?>
+			</table>
+		</div>
+		
+		<p>	
+			<div class="form-group">
+				<?php echo Html::submitButton(Yii::t('Company', 'Create'), ['class' => 'btn btn-success']); ?>
+			</div>
+		</p>
+		<?php ActiveForm::end(); ?>
 	</div>
 	
 </div>
