86f135d31aa731c8855b58ed0af51a4305a9bf51
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-03-05 15:59:26 +0200

    Add an error for failed saving

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -37,10 +37,10 @@ class SiteController extends Controller
 
 	public function actionIndex()
 	{
-		$this->tokenKey = new TokenKey();
-		$this->tokenKey->load($_POST);
-
-		$tokenKey = $this->tokenKey;
+		$tokenKey = new TokenKey();
+		$tokenKey->load($_POST);
+		$action = 'index';
+		
 		# Token key has been sent
 		if($tokenKey->token_key){
 			$record = $this->getReclaimedTokenKey($tokenKey);
@@ -48,19 +48,44 @@ class SiteController extends Controller
 			# Check if the token key is valid
 			if($this->validateTokenKey($tokenKey->token_key) === true){
 				$models = $this->getCreateCompanyModels();
-				return $this->render('create', $models);
+				
+				if($models['company']['attributes']['name']){
+					$action = 'save';
+				} else {
+					$action = 'create';
+				}
+				
 			} elseif($record == true) {
 				# Token key is already used
 				$tokenKey->addError('token_key', Yii::t('TokenKey', 'Token key is already used on {dateTime}.', ['dateTime'=>$record->reclaim_date] ));
+			
 			}
 			else{
 				# Invalid token key
 				$tokenKey->addError('token_key', Yii::t('TokenKey', 'Invalid token key.'));
+			
 			}
 		}
 		
+		# Token key is valid
+		if($action == 'create'){
+			return $this->render('create', $models);
+		}
+		
+		# Company has been sent
+		if($action == 'save'){
+			if($this->saveModels() === true){
+				return $this->render('view', $models);
+			}
+			else {
+				$tokenKey->addError('token_key', Yii::t("TokenKey", "Error while saving the company. Please report this incident to helpdesk@futurality.fi"));
+				$action = 'index';
+			}
+		}
 		# Token key has not been sent
-		return $this->render('index', array('tokenKey' => $tokenKey));
+		if($action == 'index'){
+			return $this->render('index', ['tokenKey' => $tokenKey]);
+		}
 	}
 	
 	private function validateTokenKey($tokenKey){
@@ -89,12 +114,16 @@ class SiteController extends Controller
 	}
 	
 	private function getCreateCompanyModels(){
-		$tokenKey = $this->tokenKey;
+		$tokenKey = new TokenKey();
+		$tokenKey->load($_POST);
+		
 		$company = new Company();
+		$company->load($_POST);
+		
 		$industry = new Industry();
+		$industry->load($_POST);
 		
 		$costBenefitCalculation = new CostbenefitCalculation();
-		
 		$costBenefitItemTypes = CostbenefitItemType::find()->orderBy('order')->all();
 		
 		$industryArray = $this->getIndustryArray();
@@ -154,5 +183,11 @@ class SiteController extends Controller
 			);
 		}
 		return $industrySetupArray;
+	}
+	
+	private function saveModels(){
+		$success = false;
+		
+		return $success;
 	}
 }
