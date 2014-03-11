431a6d03d87c0804d8d4714cc5dcf719663faa48
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-03-11 11:43:21 +0200

    Add default config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-	'username' => 'root',
-	'password' => '',
+	'dsn' => 'mysql:host=localhost;dbname=futural_core',
+	'username' => 'futural_core',
+	'password' => 'futural_core',
 	'charset' => 'utf8',
 ];
