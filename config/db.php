5f77805738ad27ac594e7127f60ee22230e8d188
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-09-25 14:46:28 +0300

    Revert the config file

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=127.0.0.1;port=3456;dbname=futural2_core',
+	'dsn' => 'mysql:host=127.0.0.1;dbname=futural2_core',
 	'username' => 'futural_core',
-	'password' => 'ZaG6!R#curAcRA24',
+	'password' => 'futural_core',
 	'charset' => 'utf8',
 ];
